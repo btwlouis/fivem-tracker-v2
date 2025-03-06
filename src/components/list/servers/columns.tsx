c6408b2ec8e5d06a90d56b026a28aeac0840e65a
btwlouis <info@onlouis.de>
2025-03-06 18:09:14 +0100

    fixes

--- src/components/list/servers/columns.tsx
+++ src/components/list/servers/columns.tsx
@@ -3,39 +3,36 @@
 import type { Server } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
-import { ArrowUpDown } from "lucide-react"
+import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { colorMap } from "@/lib/utils";
 
 export const columns: ColumnDef<Server>[] = [
   {
-    accessorKey: "iconVersion",
-    header: "",
-    cell: (data) => {
-      return (
-        <Image
-          src={`https://servers-frontend.fivem.net/api/servers/icon/${data.row.original.id}/${data.row.original.iconVersion}.png`}
-          width={32}
-          height={32}
-          alt="server icon"
-        />
-      );
-    },
-  },
-  {
     accessorKey: "projectName",
     header: "Project Name",
-    cell: (data) => {
-      const formattedProjectName = data?.row?.original?.projectName?.replace(
-        /\^(\d)/g,
-        (_, code: string) => `<span style='color: ${colorMap[`^${code}`] || "inherit"}'>`
-      ) + "</span>";
-  
+    cell: ({row}) => {
+      const formattedProjectName =
+        row?.original?.projectName?.replace(
+          /\^(\d)/g,
+          (_, code: string) =>
+            `<span style='color: ${colorMap[`^${code}`] || "inherit"}'>`
+        ) + "</span>";
+
       return (
         <div className="flex items-center space-x-2">
-          <div className="truncate" dangerouslySetInnerHTML={{ __html: formattedProjectName }} />
-          <div className="truncate w-96">
-            {data.row.original.projectDescription}
+          <Image
+            src={`https://servers-frontend.fivem.net/api/servers/icon/${row.original.id}/${row.original.iconVersion}.png`}
+            width={32}
+            height={32}
+            alt="server icon p-0"
+          />
+          <div
+            className="truncate"
+            dangerouslySetInnerHTML={{ __html: formattedProjectName }}
+          />
+          <div className="truncate w-96 opacity-70">
+            {row.original.projectDescription}
           </div>
         </div>
       );
@@ -62,12 +59,11 @@ export const columns: ColumnDef<Server>[] = [
       return (
         <Button
           variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
           Players
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
-      )
+      );
     },
     cell: (data) => {
       return (
@@ -77,6 +73,6 @@ export const columns: ColumnDef<Server>[] = [
           <span>{data.row.original.playersMax}</span>
         </div>
       );
-    }
+    },
   },
 ];
