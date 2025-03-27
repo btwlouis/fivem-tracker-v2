d8553c9e7b5802533bf7d60a879a73e232ee9d47
Louis <56355239+btwlouis@users.noreply.github.com>
2025-03-27 12:36:41 +0100

    fix: history data only 7 days

--- src/services/fivem.service.ts
+++ src/services/fivem.service.ts
@@ -270,9 +270,9 @@ export async function getServers() {
 }
 
 export async function deleteOldServers() {
-  // delete history where older then 2 wekk
-  const twoWeeksAgo = new Date();
-  twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
+  // delete history where older then 1 week
+  const oneWeeksAgo = new Date();
+  oneWeeksAgo.setDate(oneWeeksAgo.getDate() - 7);
 
   await prisma.serverHistory.deleteMany({
     where: {
